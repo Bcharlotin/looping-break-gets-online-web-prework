324aa73207f07cc4991ecfec6445fe427b716421
biancacharlotin <biancacharlotin@gmail.com>
2018-11-05 16:04:56 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,9 +1,8 @@
 
 def levitation_quiz
-puts "What is the spell that enacts levitation?"
-  
+  puts "What is the spell that enacts levitation?"
   answer= get.chomp
-break if answer== ""
+  break if answer== "Wingardium Leviosa"
 end
 
 
